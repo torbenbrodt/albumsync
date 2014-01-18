5496ef926287e83dfa23543860c6725b5cffd119
Torben Brodt <t.brodt@gmail.com>
2014-01-18 13:43:30 +0100

    go on with sync test, but how to find object in list?

--- controller/Sync.py
+++ controller/Sync.py
@@ -1,5 +1,3 @@
-from controller.SyncAlbum import SyncAlbum
-
 
 class Sync:
     """Controller: compares service1 (e.g. local) with service2 (e.g. picasa) and controls up- and download."""
@@ -9,15 +7,17 @@ class Sync:
         self.service_target = service_target
 
     def run(self):
-        albums_target = self.service_target.Album.Album.fetch_all()
-        album_target_match_names = lambda x: x.get_matching_name, albums_target
+        albums_targets = self.service_target.Album.Album.fetch_all()
+        album_target_match_names = map(lambda x: x.get_match_name(), albums_targets)
         for album_src in self.service_src.Album.Album.fetch_all():
-            print "matching album ", album_src.get_match_name()
+            print album_src.get_match_name()
             if album_src.get_match_name() in album_target_match_names:
-                print "match album found"
+                print "\tMATCH ALBUM FOUND, use ref"
+                # todo how to find ref
                 self.sync()
             else:
-                print "no match album found"
+                print "\tno match album found, create()"
+                album_target = self.service_target.Album.Album.create(album_src)
                 self.sync()
 
     def sync(self):
--- test/controller/SyncTest.py
+++ test/controller/SyncTest.py
@@ -17,13 +17,20 @@ class SyncTest(unittest.TestCase):
         parser.read(os.path.join(os.path.expanduser('~'), "albumsync.ini"))
         service.picasa.Config.Config.username = parser.get('credentials', 'username')
         service.picasa.Config.Config.password = parser.get('credentials', 'password')
+        # create some local folders, level 1
+        os.makedirs(service.local.Config.Config.dir + '/toplevel')
+        open(service.local.Config.Config.dir + '/toplevel/fileA.jpg', 'a').close()
+        os.makedirs(service.local.Config.Config.dir + '/2014/01 Big Data Beers')
+        open(service.local.Config.Config.dir + '/2014/01 Big Data Beers/fileB.jpg', 'a').close()
+        # create some remote albums
+        pass
 
     def test_run_from_picasa_to_local(self):
         # todo, disable imports and load automatically from Sync class
         # module = __import__('service.local', fromlist=['Album'])
         # getattr(module, 'Album')
-        import service.local.Album
         import service.picasa.Album
+        import service.local.Album
 
         sync = Sync(service.picasa, service.local)
         sync.run()
