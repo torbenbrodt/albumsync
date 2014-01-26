661f855e0274fa6ddee73f9b0982b636fd1f0fcc
Torben Brodt <t.brodt@gmail.com>
2014-01-26 21:11:45 +0100

    integrate album index and delete in Sync controller

--- controller/Front.py
+++ controller/Front.py
@@ -1,19 +1,47 @@
 import argparse
+import logging
+from controller.Sync import Sync
 
 
 class Front:
-
     def __init__(self):
         pass
 
     @staticmethod
     def run():
+        # example
+        # albumsync.py --service.picasa.user albumsync.test@googlemail.com --service.picasa.password xxx
+        # --service.local.dir ~/Pictures --util.index.dir ~/.albumsync
+        # --sync --service_src picasa --service_target local
+
         parser = argparse.ArgumentParser()
-        parser.add_argument("-u", "--username", help="Your picassaweb username")
-        parser.add_argument("-p", "--password", help="Your picassaweb password", default=False)
+
+        parser.add_argument('--service.picasa.user', help='Your picasa username')
+        parser.add_argument('--service.picasa.password', help='Your picasa password')
+
+        parser.add_argument('--service.local.dir', help='Directory where local images are stored')
+
+        # index is optional, use it to allow file deletion
+        parser.add_argument('--util.index.dir', help='Directory where the index is written')
+
+        # super config
+        parser.add_argument('--log', help='possible values are DEBUG, INFO, WARN')
+        parser.add_argument('--allowdelete', help='are deletion commando', type=bool)
+
+        # actions
+        parser.add_argument('--purge', help='Drop all index files after run', type=bool)
+        parser.add_argument('--sync', help='Drop all index files')
 
         # parse
         args = parser.parse_args()
 
-        # usage
-        verbose = args.verbose
+        # set log level
+        numeric_level = getattr(logging, args.log.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % args.log)
+        logging.basicConfig(level=numeric_level)
+
+        # todo load modules
+        sync = Sync()
+        sync.run()
+
--- controller/Sync.py
+++ controller/Sync.py
@@ -1,5 +1,6 @@
 import logging
 from controller.SyncAlbum import SyncAlbum
+import util.index.Album
 
 
 class Sync:
@@ -11,6 +12,19 @@ class Sync:
 
     def run(self):
         albums_targets_dict = dict((album_target.get_match_name(), album_target) for album_target in self.service_target.Album.Album.fetch_all())
+
+        # check against the index to see if any albums were deleted on source service
+        index = util.index.Album.Album(self.service_src)
+        index.update()
+        for album_src in index.fetch_all_deleted():
+            if album_src.get_match_name() in albums_targets_dict:
+                logging.getLogger().info(album_src.get_match_name() + ', delete album match: yes')
+                media_target = albums_targets_dict[album_src.get_match_name()]
+                media_target.delete()
+            else:
+                logging.getLogger().info(album_src.get_match_name() + ', delete album match: no')
+
+        # upload new media
         for album_src in self.service_src.Album.Album.fetch_all():
             if album_src.get_match_name() in albums_targets_dict:
                 logging.getLogger().info(album_src.get_match_name() + ', album match: yes')
